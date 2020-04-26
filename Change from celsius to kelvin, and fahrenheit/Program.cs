db67467cf64ee12cef19bc585e2a28fa21d89d63
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:16:08 +0200

    Add files via upload

---
+++ Change from celsius to kelvin, and fahrenheit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Change_from_celsius_to_kelvin__and_fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsiusf, kalvin, fahrenheit;
+            Console.WriteLine("Enter how worm its out side in celsius : ");
+            celsiusf = Convert.ToSingle(Console.ReadLine());
+
+            kalvin = celsiusf + 273;
+            fahrenheit = celsiusf + 32;
+
+            Console.WriteLine(celsiusf + " Celsius convert to Kalvin is = :" + kalvin);
+            Console.WriteLine(celsiusf + " Celsius converted to Fahrenheit is = :" + fahrenheit);
+        }
+    }
+}
